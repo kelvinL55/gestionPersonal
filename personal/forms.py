24a1644a55f23826425a37a2fd495cf7334c3579
kelvinL55 <kaloaiza1@utpl.edu.ec>
2025-03-24 19:39:28 -0500

    feat: Agregar mensajes de error personalizados en formularios de Personal y Usuario

--- personal/forms.py
+++ personal/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Personal
 
 class PersonalForm(forms.ModelForm):
+    """
+    Formulario para la creación y edición de registros de Personal
+    """
     class Meta:
         model = Personal
         fields = ['codigo', 'nombre', 'ubicacion', 'grupo', 'categorias']
@@ -13,4 +16,8 @@ class PersonalForm(forms.ModelForm):
             'grupo': 'G - GRUPO',
             'categorias': 'Categorías',
         }
-        
+        error_messages = {
+            'codigo': {
+                'unique': 'Ya existe un registro con este código en el sistema.'
+            }
+        }
